33bda4d80ae2ddf382abcdbbc8f0c1cb5e9237ce
Nolan <tranfc911@gmail.com>
2023-09-14 21:00:40 +0700

    Create Areas admin

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -105,6 +105,9 @@ namespace TeduCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
            
         }

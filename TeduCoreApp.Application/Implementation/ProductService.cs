768a16f31c4c6b66103ec43011c84a6e09959e9a
Nolan <tranfc911@gmail.com>
2023-09-24 00:34:06 +0700

    Phân trang ( AJAX )

--- TeduCoreApp.Application/Implementation/ProductService.cs
+++ TeduCoreApp.Application/Implementation/ProductService.cs
@@ -6,6 +6,7 @@ using System.Text;
 using TeduCoreApp.Application.Interfaces;
 using TeduCoreApp.Application.ViewModels.Product;
 using TeduCoreApp.Data.IRepositories;
+using TeduCoreApp.Utilities.Dtos;
 
 namespace TeduCoreApp.Application.Implementation
 {
@@ -24,6 +25,31 @@ namespace TeduCoreApp.Application.Implementation
         public List<ProductViewModel> GetAll()
         {
             return _productRepository.FindAll( x=>x.ProductCategory ).ProjectTo<ProductViewModel>().ToList();
+        }
+
+        public PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize)
+        {
+            var querey = _productRepository.FindAll(x => x.Status == Data.Enums.Status.Active);
+            if (!string.IsNullOrEmpty(keyword))
+                querey = querey.Where(x => x.Name.Contains(keyword));
+            if (categoryId.HasValue)
+                querey = querey.Where(x => x.CategoryId == categoryId.Value);
+
+            int totalRow = querey.Count();
+
+            querey = querey.OrderByDescending(x => x.DateCreated)
+                .Skip((page - 1) * pageSize).Take(pageSize);
+
+            var data = querey.ProjectTo<ProductViewModel>().ToList();
+
+            var paginationSet = new PagedResult<ProductViewModel>()
+            {
+                Results = data,
+                CurrentPage = page,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
+            return paginationSet;
         }
     }
 }
--- TeduCoreApp/wwwroot/app/controllers/product/index.js
+++ TeduCoreApp/wwwroot/app/controllers/product/index.js
@@ -1,38 +1,76 @@
-﻿var productController = function() {
+﻿var productController = function () {
     this.initialize = function () {
         loadData();
+        registerEvents();
     }
     function registerEvents() {
         //Todo Binding events to controls
+        $('#ddlShowPage').on('change', function () {
+            shophoa.configs.pageSize = $(this).val();
+            shophoa.configs.pageIndex = 1;
+            loadData(true);
+        });
     }
-    function loadData() {
+    function loadData(isPageChanged) {
         var template = $('#table-template').html();
         var render = "";
         $.ajax({
             type: 'GET',
-            url: '/admin/product/GetAll',
+            data: {
+              
+                categoryId: null,
+                keyword: $('#txtKeyword').val(),
+                page: shophoa.configs.pageIndex,
+                pageSize: shophoa.configs.pageSize
+            },
+            url: '/admin/product/GetAllPaging',
             dataType: 'json',
             success: function (response) {
-                $.each(response, function (i, item) {
+                console.log(response);
+                $.each(response.Results, function (i, item) {
                     render += Mustache.render(template, {
                         Id: item.Id,
                         Name: item.Name,
-                        Price: item.Price,     
-                        CreatedDate: item.DateCreated,
-                        Status: item.Status,
+                        Image: item.Image == null ? '<img src="/admin-side/images/user.png" width=25' : '<img src="' + item.Image + '" width=25 />',
                         CategoryName: item.ProductCategory.Name,
-                        Image: item.Image == null ? '<img src="/admin-side/images/user.png" width = 25 ' : '<img src="' + item.Image+ '"width = 25 />'        
+                        Price: shophoa.formatNumber(item.Price, 0),
+                        CreatedDate: shophoa.dateTimeFormatJson(item.DateCreated),
+                        Status: shophoa.getStatus(item.Status)
                     });
-                    if (render != '') 
-                    {
+                    $('#lblTotalRecords').text(response.RowCount);
+                    if (render != '') {
                         $('#tbl-content').html(render);
                     }
-
+                    wrapPaging(response.RowCount, function () {
+                        loadData();
+                    }, isPageChanged);
                 });
             },
             error: function (status) {
                 console.log(status);
                 shophoa.notify('Cannot loading data', 'error');
+            }
+        });
+    }
+    function wrapPaging(recordCount, callBack, changePageSize) {
+        var totalsize = Math.ceil(recordCount / shophoa.configs.pageSize);
+        //Unbind pagination if it existed or click change pagesize
+        if ($('#paginationUL a').length === 0 || changePageSize === true) {
+            $('#paginationUL').empty();
+            $('#paginationUL').removeData("twbs-pagination");
+            $('#paginationUL').unbind("page");
+        }
+        //Bind Pagination Event
+        $('#paginationUL').twbsPagination({
+            totalPages: totalsize,
+            visiblePages: 7,
+            first: 'Đầu',
+            prev: 'Trước',
+            next: 'Tiếp',
+            last: 'Cuối',
+            onPageClick: function (event, p) {
+                shophoa.configs.pageIndex = p;
+                setTimeout(callBack(), 200);
             }
         });
     }
@@ -46,33 +84,33 @@
 //        //todo: binding events to controls
 //    }
 
-//    function loadData() {
-//        var template = $('#table-template').html();
-//        var render = "";
-//        $.ajax({
-//            type: 'GET',
-//            url: '/admin/product/GetAll',
-//            dataType: 'json',
-//            success: function (response) {
-//                $.each(response, function (i, item) {
-//                    render += Mustache.render(template, {
-//                        Id: item.Id,
-//                        Name: item.Name,
-//                        Image: item.Image == null ? '<img src="/admin-side/images/user.png" width=25' : '<img src="' + item.Image + '" width=25 />',
-//                        CategoryName: item.ProductCategory.Name,
-//                        Price: shophoa.formatNumber(item.Price, 0),
-//                        CreatedDate: shophoa.dateTimeFormatJson(item.DateCreated),
-//                        Status: shophoa.getStatus(item.Status)
-//                    });
-//                    if (render != '') {
-//                        $('#tbl-content').html(render);
-//                    }
-//                });
-//            },
-//            error: function (status) {
-//                console.log(status);
-//                tedu.notify('Cannot loading data', 'error');
-//            }
-//        })
-//    }
+    //function loadData() {
+    //    var template = $('#table-template').html();
+    //    var render = "";
+    //    $.ajax({
+    //        type: 'GET',
+    //        url: '/admin/product/GetAll',
+    //        dataType: 'json',
+    //        success: function (response) {
+    //            $.each(response, function (i, item) {
+    //                render += Mustache.render(template, {
+    //                    Id: item.Id,
+    //                    Name: item.Name,
+    //                    Image: item.Image == null ? '<img src="/admin-side/images/user.png" width=25' : '<img src="' + item.Image + '" width=25 />',
+    //                    CategoryName: item.ProductCategory.Name,
+    //                    Price: shophoa.formatNumber(item.Price, 0),
+    //                    CreatedDate: shophoa.dateTimeFormatJson(item.DateCreated),
+    //                    Status: shophoa.getStatus(item.Status)
+    //                });
+    //                if (render != '') {
+    //                    $('#tbl-content').html(render);
+    //                }
+    //            });
+    //        },
+    //        error: function (status) {
+    //            console.log(status);
+    //            tedu.notify('Cannot loading data', 'error');
+    //        }
+    //    })
+    //}
 //}
